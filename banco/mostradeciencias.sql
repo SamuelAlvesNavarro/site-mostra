773c3184944e10e5c6a3b54c58809cb3e35ca693
Davi <121810059+Davi1544@users.noreply.github.com>
2023-10-17 08:58:12 -0300

    Update mostradeciencias.sql
    eeee

--- banco/mostradeciencias.sql
+++ banco/mostradeciencias.sql
@@ -1,79 +1,16 @@
--- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: mostradeciencias
--- ------------------------------------------------------
--- Server version	10.4.28-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `animais`
---
-
-DROP TABLE IF EXISTS `animais`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `animais` (
+CREATE TABLE `animal` (
   `id_animal` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
-  `qtnd_votos` int(11) NOT NULL,
+  `pasta_nome` varchar(255) NOT NULL,
   `descr` text NOT NULL,
   PRIMARY KEY (`id_animal`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `animais`
---
-
-LOCK TABLES `animais` WRITE;
-/*!40000 ALTER TABLE `animais` DISABLE KEYS */;
-INSERT INTO `animais` VALUES (1,'Macaco',0,'É um macaco');
-/*!40000 ALTER TABLE `animais` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `votos`
---
-
-DROP TABLE IF EXISTS `votos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `votos` (
+CREATE TABLE `voto` (
   `id_voto` int(11) NOT NULL AUTO_INCREMENT,
-  `fk_id_voto` int(11) DEFAULT NULL,
-  `hora` date DEFAULT NULL,
+  `fk_id_animal` int(11) NOT NULL,
+  `hora` date DEFAULT now(),
   PRIMARY KEY (`id_voto`),
-  KEY `fk_id_voto` (`fk_id_voto`),
-  CONSTRAINT `votos_ibfk_1` FOREIGN KEY (`fk_id_voto`) REFERENCES `animais` (`id_animal`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  CONSTRAINT `votos_ibfk_1` FOREIGN KEY (`fk_id_animal`) REFERENCES `animal` (`id_animal`)
+);
 
---
--- Dumping data for table `votos`
---
-
-LOCK TABLES `votos` WRITE;
-/*!40000 ALTER TABLE `votos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `votos` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-07 17:41:24
