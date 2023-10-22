9ae78223dd3906d626504a9c854c9fde21900d33
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-10-22 11:52:18 -0300

    votos

--- site/voto.php
+++ site/voto.php
@@ -1,3 +1,11 @@
 <?php
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=mostradeciencias', 'root', '');
 
+    $id_animal = $_POST['id_animal'];
+
+    $sql = "INSERT INTO voto VALUES(NULL, '$id_animal',now())";
+    $prepare = $pdo->prepare($sql);
+    $prepare->execute();
+
+    header("Location:pesquisa.php");
 ?>
